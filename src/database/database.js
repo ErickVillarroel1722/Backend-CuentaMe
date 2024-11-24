45982771d8b3f4ed225cb7d3c15dce04fb8a011c
Erick Villarroel <erickvillarroel0022@gmail.com>
2024-11-24 18:34:42 -0500

    Definición de controladores y rutas para la Orden de compra, además de la modificación del estado de la misma.

--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,12 @@
 import mongoose from 'mongoose'
 
-const MONGO_URI = process.env.MONGO_URI
 const ATLAS_URI = process.env.ATLAS_URI
 
 mongoose.set('strictQuery', true)
 
 const connection = async() => {
   try{
-    const {connection} = await mongoose.connect(MONGO_URI)
+    const {connection} = await mongoose.connect(ATLAS_URI)
     console.log(`Database has established on MongoDB server as ${connection.host} - ${connection.port}`)
     console.log("-----------------------------------------------------------------------\n")
   }
